bf4f8b18bc2935240845156f7dd4be29a8c4a398
thevi31415 <92256900+thevi31415@users.noreply.github.com>
2022-12-20 19:38:00 +0700

    first commit

---
+++ LeftNavigation/app/src/main/java/com/example/leftnavigation/NotFragment.kt
@@ -0,0 +1,20 @@
+package com.example.leftnavigation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class NotFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_not, container, false)
+    }
+
+
+}
